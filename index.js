31309c60d13e230bfa5b239e4dae187c7b7577d1
Ben Harrop <bjh503@gmail.com>
2014-07-06 16:34:56 +0100

    removed trash

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-/*var nodeUtil = require("util"),
-            fs = require('fs'),
-            _ = require('underscore'),
-            PDFParser = require("pdf2json/pdfparser");
-
-var ben = function(stuff){
-
-    console.log(stuff.data);
-
-    if(stuff.data.Pages){
-        for (var i = 0; i < stuff.data.Pages.length; i++) {
-            var page = stuff.data.Pages[i];
-
-            for (var j = 0; j < page.Texts.length; j++) {
-                var text = page.Texts[j];
-
-                for (var k = 0; k < text.R.length; k++) {
-                    var something = text.R[k];
-
-                    console.log(decodeURIComponent(something.T));
-                };
-            };
-        };
-    }
-}
-
-        var pdfParser = new PDFParser();
-
-        pdfParser.on("pdfParser_dataReady", _.bind(ben, pdfParser));
-
-        pdfParser.on("pdfParser_dataError", _.bind(ben, pdfParser));
-
-        var pdfFilePath = "burger2.pdf";
-
-        pdfParser.loadPDF(pdfFilePath);*/
-
 var foodstuff = require('./foodstuff');
 
 foodstuff.parsePDF('burger.pdf');
